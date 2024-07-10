2d07c12e9f7fd3abb2e2827a320e1b7a8683e77a
Deepramakhal <deepra313131@gmail.com>
2024-07-10 11:09:33 +0530

    Mobile touch on menu bar feature added

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 var expandButton = document.getElementById('expand-button')
 var navSection =  document.getElementById('nav-2')
 var menuIcon = document.getElementById('menu-icon')
-var clickCounter = 0;
+let clickCounter = 0;
+let touchCounter = 0;
 
 expandButton.addEventListener("click", function(){
     if (clickCounter == 0){
+        navSection.style.display = 'block';
+        menuIcon.setAttribute("style","rotate:90deg");
+        clickCounter++;
+    }
+    else{
+        navSection.style.display = 'none';
+        menuIcon.setAttribute("style","rotate:0");
+        clickCounter--;
+    }
+})
+
+expandButton.addEventListener("touchcancel",function(){
+    if (touchCounter == 0){
         navSection.style.display = 'block';
         menuIcon.setAttribute("style","rotate:90deg");
         clickCounter++;
